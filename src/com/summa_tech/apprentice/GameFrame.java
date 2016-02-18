281f929f81b85fee6b16dd85cb96f629788dcf90
jchilleo <jchilleo2011@gmail.com>
2016-02-18 13:33:31 -0500

    gameFrame object
    Added the GameFrame object file, with getters and setters for all the
    fields.
    
    The object contains fields for
    Bonus, first/second/third roll, frame total before bonuses, frame total,
    and also current overall total.

---
+++ src/com/summa_tech/apprentice/GameFrame.java
@@ -0,0 +1,130 @@
+package com.summa_tech.apprentice;
+
+
+/**
+ * 
+ * @author jchilleo
+ * This is the game frame object, which will hold the score values in addition to totals/bonus data.
+ */
+public class GameFrame {
+	
+	/*
+	 * score for each roll in a frame.
+	 * Third is only used in the final 10th frame, when applicable.
+	 */
+	private byte firstRoll = 0, secondRoll = 0, thirdRoll=0;
+	/*
+	 * frameTotal = frame score value with bonuses added.
+	 * framePBTotal = frame score pre-bonus value. 
+	 */
+	private byte frameTotal = 0, framePBTotal = 0;
+	// currentTotal = total of this frame plus all previous frames.
+	private short currentTotal = 0;
+	/*
+	 * bonus indicates if the frame contained a strike, spare, none.
+	 * stirke = X or x
+	 * spare = S or s
+	 * none = N or n
+	 */
+	private char bonus = 'n';
+	
+	/**
+	 * Constructor for frames 1-9
+	 * @param firstRoll - value for first roll in frame.
+	 * @param secondRoll - value for second roll in frame.
+	 */
+	public void GameFrame(byte firstRoll, byte secondRoll){
+		this.firstRoll = firstRoll;
+		this.secondRoll = secondRoll;
+	}
+	
+	/**
+	 * Constructor for 10th frame
+	 * @param firstRoll - value for first roll in frame.
+	 * @param secondRoll - value for second roll in frame.
+	 * @param thirdRoll - value for third roll in frame.
+	 */
+	public void GameFrame(byte firstRoll, byte secondRoll, byte thirdRoll){
+		this.firstRoll = firstRoll;
+		this.secondRoll = secondRoll;
+		this.thirdRoll = thirdRoll;
+	}
+	
+	/**
+	 * @return value for firstRoll
+	 */
+	public byte getFirstRoll(){
+		return firstRoll;
+	}
+	
+	/**
+	 * @return value for firstRoll
+	 */
+	public byte getSecondRoll(){
+		return secondRoll;
+	}
+	
+	/**
+	 * @return value for firstRoll
+	 */
+	public byte getThirdRoll(){
+		return thirdRoll;
+	}
+	
+	/**
+	 * @return value for frame total with bonuses added.
+	 */
+	public byte getFrameTotal(){
+		return frameTotal;
+	}
+	
+	/**
+	 * @return value for frame total before bonuses.
+	 */
+	public byte getFramePBTotal(){
+		return framePBTotal;
+	}
+	
+	/**
+	 * @return value for current total of preceding frames and this frame.
+	 */
+	public short getCurrentTotal(){
+		return currentTotal;
+	}
+	
+	/**
+	 * @return bonus indicator (strike, spare, none)
+	 */
+	public char getBonus(){
+		return bonus;
+	}
+	
+	/**
+	 * @param frameTotal - value of frameTotal with bonuses
+	 */
+	public void setFrameTotal(byte frameTotal){
+		this.frameTotal = frameTotal;
+	}
+	
+	/**
+	 * @param framePBTotal - value of frame total before bonuses.
+	 */
+	public void setFramePBTotal(byte framePBTotal){
+		this.framePBTotal = framePBTotal;
+	}
+	
+	/**
+	 * @param currentTotal - value of frame total including preceding frames.
+	 */
+	public void setCurrentTotal(short currentTotal){
+		this.currentTotal = currentTotal;
+	}
+	
+	/**
+	 * @param bonus - status marker for bonus.
+	 */
+	public void setBonus(char bonus){
+		this.bonus = bonus;
+	}
+	
+}
---
+++ src/com/summa_tech/apprentice/Main.java
@@ -0,0 +1,18 @@
+package com.summa_tech.apprentice;
+
+/**
+ * 
+ * @author Justin Chilleo
+ * This program will take an array of integers as input to calculate a bowling score total.
+ * The program will support an array of size 10 or 11 total numbers.
+ * If the input array does not contain at least 10 numbers, the program will auto-fill the array with zeros.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
